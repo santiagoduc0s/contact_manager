5646f15c21337f713fd2ae336bb204cda1b54727
santiagoduc0s <santidu200@gmail.com>
2024-11-03 02:16:21 +0000

    feat(ui): not rotate the app anymore

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:contacts_manager/features/home/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
