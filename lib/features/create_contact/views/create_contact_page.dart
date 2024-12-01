f5ac8085e5159bc929346369fc1e3212a26fa869
santiagoduc0s <santidu200@gmail.com>
2024-12-01 23:20:19 +0000

    feat(forms): add reactive_forms and use in create contact screen

--- lib/features/create_contact/views/create_contact_page.dart
+++ lib/features/create_contact/views/create_contact_page.dart
@@ -1,26 +1,19 @@
 import 'package:contacts_manager/alerts/alerts.dart';
 import 'package:contacts_manager/cruds/cruds.dart';
+import 'package:contacts_manager/ui/ui_spacing.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:reactive_forms/reactive_forms.dart';
 
-class CreateContactPage extends StatefulWidget {
+class CreateContactPage extends StatelessWidget {
   const CreateContactPage({super.key});
-
-  @override
-  State<CreateContactPage> createState() => _CreateContactPageState();
-}
-
-class _CreateContactPageState extends State<CreateContactPage> {
-  final TextEditingController _firstNameController = TextEditingController();
-  final TextEditingController _lastNameController = TextEditingController();
-  final TextEditingController _phoneController = TextEditingController();
 
   Future<void> _saveContact(BuildContext context) async {
     try {
       final contact = await ContactCrud.instance.create(
-        firstName: _firstNameController.text,
-        lastName: _lastNameController.text,
-        phoneNumber: _phoneController.text,
+        firstName: form.control('firstName').value,
+        lastName: form.control('lastName').value,
+        phoneNumber: form.control('phone').value,
       );
 
       CustomSnackbar.success(text: '${contact.name.first} was created');
@@ -34,13 +27,20 @@ class _CreateContactPageState extends State<CreateContactPage> {
     }
   }
 
-  @override
-  dispose() {
-    _firstNameController.dispose();
-    _lastNameController.dispose();
-    _phoneController.dispose();
-    super.dispose();
-  }
+  FormGroup get form => FormGroup({
+        'firstName': FormControl<String>(
+          value: '',
+          validators: [Validators.required],
+        ),
+        'lastName': FormControl<String>(
+          value: '',
+          validators: [],
+        ),
+        'phone': FormControl<String>(
+          value: '',
+          validators: [Validators.required],
+        ),
+      });
 
   @override
   Widget build(BuildContext context) {
@@ -50,72 +50,84 @@ class _CreateContactPageState extends State<CreateContactPage> {
         title: const Text('Create Contact'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _firstNameController,
-              decoration: InputDecoration(
-                labelText: 'First Name',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.deepPurple),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-            ),
-            const SizedBox(height: 15),
-            TextField(
-              controller: _lastNameController,
-              decoration: InputDecoration(
-                labelText: 'Last Name',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.deepPurple),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-            ),
-            const SizedBox(height: 15),
-            TextField(
-              controller: _phoneController,
-              decoration: InputDecoration(
-                labelText: 'Phone Number',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.deepPurple),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-              keyboardType: TextInputType.phone,
-            ),
-            const SizedBox(height: 10),
-            Row(
-              children: [
-                Expanded(
-                  child: FilledButton(
-                    onPressed: () => _saveContact(context),
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5.0),
+        padding: const EdgeInsets.all(UISpacing.space4x),
+        child: ReactiveFormBuilder(
+            form: () => form,
+            builder: (context, form, child) {
+              return Column(
+                children: [
+                  ReactiveTextField<String>(
+                    formControlName: 'firstName',
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(
+                      labelText: 'First Name',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(UISpacing.space3x),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.deepPurple),
+                        borderRadius: BorderRadius.circular(UISpacing.space3x),
                       ),
                     ),
-                    child: const Text(
-                      'Save Contact',
-                      style: TextStyle(fontSize: 15),
+                  ),
+                  const SizedBox(height: UISpacing.space4x),
+                  ReactiveTextField<String>(
+                    formControlName: 'lastName',
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(
+                      labelText: 'Last Name',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(UISpacing.space3x),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.deepPurple),
+                        borderRadius: BorderRadius.circular(UISpacing.space3x),
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
-        ),
+                  const SizedBox(height: UISpacing.space4x),
+                  ReactiveTextField<String>(
+                    formControlName: 'phone',
+                    textInputAction: TextInputAction.done,
+                    decoration: InputDecoration(
+                      labelText: 'Phone Number',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(UISpacing.space3x),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.deepPurple),
+                        borderRadius: BorderRadius.circular(UISpacing.space3x),
+                      ),
+                    ),
+                  ),
+                  const Spacer(),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: FilledButton(
+                          onPressed: () {
+                            if (form.valid) {
+                              _saveContact(context);
+                            } else {
+                              form.markAllAsTouched();
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                            ),
+                          ),
+                          child: const Text(
+                            'Save Contact',
+                            style: TextStyle(fontSize: 15),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              );
+            }),
       ),
     );
   }
